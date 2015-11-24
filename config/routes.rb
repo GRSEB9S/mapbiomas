538c21d0b844faee8edc60ad9f9d825b0e29b297
karydja <karydjasouza@outlook.com>
2015-11-24 15:41:44 -0300

    "Methodology" content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'map#index'
 
+  get 'pages/methodology', to: 'pages#methodology', as: 'pages_methodology'
+
   namespace :api, defaults: { format: :json } do
     resources :classifications, only: :index
     resources :coverage, only: :index
