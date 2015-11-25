bdcc76c1ea71d41a505754fbdf765f8550ff1a0b
karydja <karydjasouza@outlook.com>
2015-11-24 23:55:16 -0300

    "About" content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def methodology
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'map#index'
 
   get 'pages/methodology', to: 'pages#methodology', as: 'pages_methodology'
+  get 'pages/about', to: 'pages#about', as: 'pages_about'
 
   namespace :api, defaults: { format: :json } do
     resources :classifications, only: :index
