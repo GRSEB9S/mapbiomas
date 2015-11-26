15d5b68ae8ea4b628ba21632b92d771117f09173
Marcelo Piva <m.pivaa@gmail.com>
2015-11-26 00:55:05 -0200

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -8,14 +8,14 @@ threads 1, 6
 environment = ENV['RAILS_ENV'] || "production"
 
 # Set up socket location
-bind "unix://#{shared_path}/sockets/puma.sock"
+bind "unix://#{shared_path}/tmp/sockets/puma.sock"
 
 # Logging
 stdout_redirect "#{shared_path}/log/puma.stdout.log", "#{shared_path}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_path}/pids/puma.pid"
-state_path "#{shared_path}/pids/puma.state"
+pidfile "#{shared_path}/tmp/pids/puma.pid"
+state_path "#{shared_path}/tmp/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
