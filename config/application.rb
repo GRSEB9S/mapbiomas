7297018d75f4ce6b6f34029ed10deb370aaa9472
karydja <karydjasouza@outlook.com>
2015-11-30 18:02:16 -0300

    Add English locales
    Some translations are still in pt-BR (we're waiting for the offical ones in English)

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,7 @@ module Mapbiomas
     config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = 'pt-BR'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = %w(pt-BR en)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
