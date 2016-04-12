cc564f778bf7c09f71104e82ff02b4b0d499d000
karydja <karydjasouza@outlook.com>
2016-04-12 11:29:59 -0300

    Fix 'about us' section

--- config/puma.rb
+++ config/puma.rb
@@ -5,15 +5,15 @@ workers 2
 threads 1, 6
 environment ENV['RAILS_ENV'] || "production"
 
-unless ENV["HEROKU"]
+if ENV["HEROKU"]
+  rackup DefaultRackup
+  port ENV['PORT'] || 3000
+else
   bind "unix://#{shared_path}/tmp/sockets/puma.sock"
   stdout_redirect "#{shared_path}/log/puma.stdout.log", "#{shared_path}/log/puma.stderr.log", true
   pidfile "#{shared_path}/tmp/pids/puma.pid"
   state_path "#{shared_path}/tmp/sockets/puma.state"
   activate_control_app
-else
-  rackup      DefaultRackup
-  port        ENV['PORT']     || 3000
 end
 
 on_worker_boot do
