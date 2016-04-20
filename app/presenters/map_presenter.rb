bbcb8f2f5e58b74ab2cfdd09c9aa3ca3d2b09365
karydja <karydjasouza@outlook.com>
2016-04-20 09:57:11 -0300

    Refactor MapPresenter#layers

--- app/presenters/map_presenter.rb
+++ app/presenters/map_presenter.rb
@@ -10,7 +10,16 @@ class MapPresenter
     biomes: '#F11810',
     indigenous_lands: '#7B00B4',
     conservation_units: '#FF6600'
-  }
+  }.freeze
+
+  LAYERS_KEYS = {
+    states:            '4dd47a54-01a8-11e6-86a9-0e31c9be1b51',
+    cities:            '72b94172-0263-11e6-a087-0e5db1731f59',
+    contour_maps:       '1413c17c-0274-11e6-ae17-0e787de82d45',
+    biomes:            '201bcb2a-026c-11e6-9f9a-0e3ff518bd15',
+    indigenous_lands:   '9294ef0a-04f2-11e6-8a00-0e31c9be1b51',
+    conservation_units: 'c6f498c2-04f2-11e6-bedf-0ecd1babdde5'
+  }.freeze
 
   def as_json(*_)
     {
@@ -43,49 +52,14 @@ class MapPresenter
   end
 
   def layers
-    [
+    LAYERS_KEYS.each_with_index.map do |(layer, key), id|
       {
-        id: 0,
-        slug: 'states',
-        name: I18n.t('map.index.layers.states'),
-        color: LAYERS_COLORS[:states],
-        link: 'https://karydja.cartodb.com/api/v2/viz/4dd47a54-01a8-11e6-86a9-0e31c9be1b51/viz.json'
-      },
-      {
-        id: 1,
-        slug: 'cities',
-        name: I18n.t('map.index.layers.cities'),
-        color: LAYERS_COLORS[:cities],
-        link: 'https://karydja.cartodb.com/api/v2/viz/72b94172-0263-11e6-a087-0e5db1731f59/viz.json'
-      },
-      {
-        id: 2,
-        slug: 'contourMaps',
-        name: I18n.t('map.index.layers.contour_maps'),
-        color: LAYERS_COLORS[:contour_maps],
-        link: 'https://karydja.cartodb.com/api/v2/viz/1413c17c-0274-11e6-ae17-0e787de82d45/viz.json'
-      },
-      {
-        id: 3,
-        slug: 'biomes',
-        name: I18n.t('map.index.layers.biomes'),
-        color: LAYERS_COLORS[:biomes],
-        link: 'https://karydja.cartodb.com/api/v2/viz/201bcb2a-026c-11e6-9f9a-0e3ff518bd15/viz.json'
-      },
-      {
-        id: 4,
-        slug: 'indigenousLands',
-        name: I18n.t('map.index.layers.indigenous_lands'),
-        color: LAYERS_COLORS[:indigenous_lands],
-        link: 'https://karydja.cartodb.com/api/v2/viz/9294ef0a-04f2-11e6-8a00-0e31c9be1b51/viz.json'
-      },
-      {
-        id: 5,
-        slug: 'conservationUnits',
-        name: I18n.t('map.index.layers.conservation_units'),
-        color: LAYERS_COLORS[:conservation_units],
-        link: 'https://karydja.cartodb.com/api/v2/viz/c6f498c2-04f2-11e6-bedf-0ecd1babdde5/viz.json'
+        id: id,
+        slug: layer.to_s.camelize(:lower),
+        name: I18n.t(layer, scope: 'map.index.layers'),
+        color: LAYERS_COLORS[layer],
+        link: "https://karydja.cartodb.com/api/v2/viz/#{key}/viz.json"
       }
-    ]
+    end
   end
 end
