b234a25c2b3aa70dcaf0e7a0149244761b68b048
Eric Prieto <ericprieto@live.com>
2017-04-25 17:28:01 -0300

    map-controls

---
+++ app/assets/javascripts/components/map/panels/zoom_and_opacity.es6.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import cx from 'classnames';
+
+class ZoomAndOpacityControl extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = { showOpacityDropdown: false };
+  }
+
+  toggleOpacityDropdown() {
+    this.setState({ showOpacityDropdown: !this.state.showOpacityDropdown });
+  }
+
+  handleOpacityChange({ target: { value } }) {
+    this.props.setOpacity(value / 100);
+  }
+
+  render() {
+    const { showOpacityDropdown } = this.state;
+    const { zoomIn, zoomOut, opacity } = this.props;
+
+    return (
+      <div className="map-panel">
+        <button className="button button--primary" onClick={zoomIn}>
+          <i className="fa fa-plus" />
+        </button>
+        <button className="button button--primary" onClick={zoomOut}>
+          <i className="fa fa-minus" />
+        </button>
+        <button
+          className={cx('button button--primary', showOpacityDropdown && 'active')}
+          onClick={this.toggleOpacityDropdown.bind(this)}
+        >
+          Opacidade
+        </button>
+        {showOpacityDropdown && (
+          <div className="zoom-and-opacity--opacity-dropdown">
+            <input
+              min="0"
+              max="100"
+              type="range"
+              style={{ width: '100%' }}
+              value={opacity * 100}
+              onChange={this.handleOpacityChange.bind(this)}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default ZoomAndOpacityControl;
