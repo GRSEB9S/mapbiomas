684ef9d6e45d2b8035f6c570b0aa54746eeec509
karydja <karydjasouza@outlook.com>
2017-04-27 22:22:34 -0300

    Create period select for Transitions tab

--- app/assets/javascripts/components/map/panels/territory.es6.jsx
+++ app/assets/javascripts/components/map/panels/territory.es6.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import Select from 'react-select';
+import Select from 'react-select-plus';
 
 const renderTabPanel = (
   category,
