0e2914e18aaa0027bcc57b2a1b189fe5ea68884b
Marcelo Piva <m.pivaa@gmail.com>
2017-04-27 21:00:07 -0300

    Stats page

---
+++ app/assets/javascripts/components/stats/stats.es6.jsx
@@ -0,0 +1,184 @@
+import React from 'react';
+import _ from 'underscore';
+import classNames from 'classnames';
+import { API } from '../../lib/api';
+import Highcharts from 'highcharts';
+import { Territories } from '../../lib/territories';
+import Select from 'react-select';
+
+class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(!_.isEqual(prevProps, this.props)) {
+      this.fetchData();
+    }
+    if(!_.isEqual(prevProps.data, this.props.data)) {
+      new Highcharts.Chart(this.refs.chart, this.buildOptions());
+    }
+  }
+
+  fetchData() {
+    API.coverage({
+      territory_id: this.props.territories.map(t => t.value).join(','),
+      classification_ids: this.props.classes.map((c) => c.value).join(',')
+    }).then(data => this.setState({ data }));
+  }
+
+  buildSeries() {
+    if(this.props.territories.length > 1) {
+      return this.props.territories.map(t => {
+        return {
+          name: t.label,
+          data: this.props.years.map(y => {
+            const d = _.find(this.state.data, { year: y, territory: t.value });
+            if(d && _.isNumber(d.area)) return d.area;
+            return 0;
+          })
+        };
+      });
+    } else {
+      return this.props.classes.map(c => {
+        return {
+          name: c.label,
+          color: c.color,
+          data: this.props.years.map(y => {
+            const d = _.find(this.state.data, { year: y, id: c.id });
+            if(d && _.isNumber(d.area)) return d.area;
+            return 0;
+          })
+        };
+      });
+    }
+  }
+
+  buildOptions() {
+    return {
+      series: this.buildSeries()
+    };
+  }
+
+  render() {
+    return (
+      <div ref="chart" />
+    );
+  }
+}
+
+export default class Stats extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedTerritories: [],
+      selectedClasses: []
+    };
+  }
+
+  onTerritoryChange(selectedTerritories) {
+    this.setState({ selectedTerritories })
+  }
+
+  onClassChange(selectedClasses) {
+    this.setState({ selectedClasses })
+  }
+
+  loadTerritories() {
+    return (input, callback) => {
+      clearTimeout(this.timeoutId);
+
+      if (input) {
+        this.timeoutId = setTimeout(() => {
+          API.territories({
+            name: input.toUpperCase()
+          })
+          .then((territories) => {
+            callback(null, {
+              options: new Territories(territories).withOptions()
+            });
+          });
+        }, 500);
+      } else {
+        callback(null, { options: [] });
+      }
+    };
+  }
+
+  getClassesOptions() {
+    return this.props.classes.map(c => ({
+      label: c.name,
+      color: c.color,
+      value: c.id
+    }));
+  }
+
+  renderCharts() {
+    if(this.state.selectedTerritories.length > 1 && this.state.selectedClasses.length > 1) {
+      return this.state.selectedTerritories.map((territory, i) =>
+        <Chart key={i} territories={[territory]} classes={this.state.selectedClasses} />);
+    } else {
+      return (
+        <Chart
+          territories={this.state.selectedTerritories}
+          classes={this.state.selectedClasses}
+        />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <article className="page">
+        <div className="page__container">
+          <h1 className="page__title">{I18n.t('stats.title')}</h1>
+         
+          <div className="stats">
+            <div className="stats__filter-box">
+              <div className="stats__filter">
+                <label>{I18n.t('stats.territories')}</label>
+                <Select.Async
+                  name="territory-select"
+                  value={this.state.selectedTerritories}
+                  loadOptions={this.loadTerritories()}
+                  onChange={this.onTerritoryChange.bind(this)}
+                  clearable={false}
+                  ignoreAccents={false}
+                  noResultsText={false}
+                  searchingText={I18n.t('stats.index.searching')}
+                  multi={true}
+                />
+              </div>
+              <div className="stats__filter">
+                <label>{I18n.t('stats.classes')}</label>
+                <Select
+                  name="class-select"
+                  value={this.state.selectedClasses}
+                  options={this.getClassesOptions()}
+                  onChange={this.onClassChange.bind(this)}
+                  clearable={false}
+                  ignoreAccents={false}
+                  noResultsText={false}
+                  searchingText={I18n.t('stats.index.searching')}
+                  multi={true}
+                />
+              </div>
+            </div>
+            <div className="stats__chart-container">
+              {this.renderCharts()} 
+            </div>
+          </div>
+        </div>
+      </article>
+    );
+  }
+}
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,9 +5,11 @@ import ReactDOM from 'react-dom';
 import LandsatDownload from './components/download/landsat_download';
 import Map from './components/map/map';
 import Menu from './components/menu/menu';
+import Stats from './components/stats/stats';
 
 window.React = React;
 window.ReactDOM = ReactDOM;
 window.LandsatDownload = LandsatDownload;
 window.Map = Map;
 window.Menu = Menu;
+window.Stats = Stats;
