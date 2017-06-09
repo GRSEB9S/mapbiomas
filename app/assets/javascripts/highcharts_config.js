daaacd7f6dd06e33ca12236d3182b7228fecb743
karydja <karydjasouza@outlook.com>
2017-06-09 14:08:20 -0300

    Set i18n for number formatting

--- app/assets/javascripts/highcharts_config.js
+++ app/assets/javascripts/highcharts_config.js
@@ -2,8 +2,8 @@ import Highcharts from 'highcharts';
 
 Highcharts.setOptions({
   lang: {
-    decimalPoint: ',',
-    thousandsSep: '.'
+    decimalPoint: I18n.t('number.format.delimiter'),
+    thousandsSep: I18n.t('number.format.delimiter')
   },
   credits: {
     enabled: false
