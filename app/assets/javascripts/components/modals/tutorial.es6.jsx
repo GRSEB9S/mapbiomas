3f4b0ddc3f48471db30fad431f450a5391ae56bd
karydja <karydjasouza@outlook.com>
2017-12-20 22:41:23 -0200

    Create tutorials for Coverage and Transitions pages

--- app/assets/javascripts/components/modals/tutorial.es6.jsx
+++ app/assets/javascripts/components/modals/tutorial.es6.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { MapModal } from './modal';
 
-const WarningModal = ({ title, html, onClose, showTutorialButton, onTutorialClick }) => (
+const TutorialModal = ({ title, html, onClose }) => (
   <MapModal
     title={title}
     showCloseButton={false}
     showOkButton={true}
-    showTutorialButton={showTutorialButton}
-    onTutorialClick={onTutorialClick}
     verticalSmaller={true}
     horizontalSmaller={true}
-    overlay={true}
     onClose={onClose}>
     <div dangerouslySetInnerHTML={{__html: html}}></div>
   </MapModal>
 );
 
-export default WarningModal;
+export default TutorialModal;
