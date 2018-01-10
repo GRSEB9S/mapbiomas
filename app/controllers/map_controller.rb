37df62b5735302b9ea3c9253e10ac642f9c2788b
karydja <karydjasouza@outlook.com>
2018-01-10 15:19:38 -0200

    Remove user authorization for index action

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,5 @@
 class MapController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :iframe]
   before_action :set_map, only: [:update, :iframe]
   respond_to :json, only: :create
 
