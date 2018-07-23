b69e62bdce3efbdaa038b6aa45a3e7341749b740
Gabriel Packer <ggkpacker@gmail.com>
2018-07-23 13:06:22 -0300

    Set env variables for discourse

---
+++ config/initializers/discourse.rb
@@ -0,0 +1,3 @@
+DISCOURSE_URL = ENV['DISCOURSE_URL'] || raise('Please set DISCOURSE_URL environment variable')
+DISCOURSE_SSO_SECRET = ENV['DISCOURSE_SSO_SECRET'] || raise('Please set DISCOURSE_SSO_SECRET environment variable')
+DISCOURSE_SSO_URL = ENV['DISCOURSE_SSO_URL'] || raise('Please set DISCOURSE_SSO_URL environment variable')
