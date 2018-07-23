aec079fc9b440e2a5a021461fa52e75669ae9366
Gabriel Packer <ggkpacker@gmail.com>
2018-07-23 13:23:34 -0300

    Add SSO controller for forum's users auth

---
+++ app/controllers/discourse_sso_sessions_controller.rb
@@ -0,0 +1,25 @@
+class DiscourseSsoSessionsController < ApplicationController
+  def authenticate
+    store_location_for :user, request.fullpath unless signed_in?
+
+    authenticate_user!
+
+    sso.email = current_user.email
+    sso.external_id = current_user.id
+
+    redirect_to sso.to_url
+  end
+
+  private
+
+  def query_string
+    raise 'No payload was provided' if request.query_string.blank?
+    request.query_string
+  end
+
+  def sso
+    @sso ||= DiscourseApi::SingleSignOn
+             .parse(query_string, DISCOURSE_SSO_SECRET)
+             .tap { |sso| sso.sso_url = DISCOURSE_SSO_URL }
+  end
+end
