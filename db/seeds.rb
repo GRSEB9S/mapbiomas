5ae35f907adfd3ae7b989405572102c7859ce84a
Gabriel Packer <ggkpacker@gmail.com>
2018-07-31 14:38:26 -0300

    Add glossarry seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+puts 'Cleaning Glossary DB'
+
+Glossary.destroy_all
+GlossaryCategory.destroy_all
+puts 'Done'
+
+puts 'Seeding DB'
+CSV.foreach("#{Rails.root}/db/data/glossary_seed.csv", {headers: true}) do |row|
+  # TODO - waiting glossary final version with category field
+  category = GlossaryCategory.find_by(name: 'teste') || GlossaryCategory.create(name: 'teste')
+  Glossary.create(word: row['Palavras'], definition: row['Definição'], glossary_category: category)
+end
+puts "Done - #{Glossary.count} words created"
