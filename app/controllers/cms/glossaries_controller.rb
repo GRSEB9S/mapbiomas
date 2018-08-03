9c53baabadc687bd40791332af426a6ec5969413
Gabriel Packer <ggkpacker@gmail.com>
2018-08-03 14:33:53 -0300

    Display form errors

--- app/controllers/cms/glossaries_controller.rb
+++ app/controllers/cms/glossaries_controller.rb
@@ -11,25 +11,27 @@ class Cms::GlossariesController < ApplicationController
     @glossary = Glossary.new(glossary_params)
     authorize @glossary
     if @glossary.save
-      redirect_to cms_glossaries_path, flash[:success] = I18n.t('glossaries.create_success')
+      redirect_to glossaries_path
     else
-      render :new, flash[:error] = I18n.t('glossaries.create_error')
+      flash.now[:error] = I18n.t('glossaries.create_error')
+      render :new
     end
   end
 
   def update
     authorize @glossary
     if @glossary.update(glossary_params)
-      redirect_to cms_glossaries_path, flash[:success] = I18n.t('glossaries.update_success')
+      redirect_to glossaries_path
     else
-      render :edit, flash[:error] = I18n.t('glossaries.update_failed')
+      flash.now[:error] = I18n.t('glossaries.update_error')
+      render :edit
     end
   end
 
   def destroy
     authorize @glossary
     @glossary.destroy
-    redirect_to cms_glossaries_path, flash[:success] = I18n.t('glossaries.destroy_success')
+    redirect_to glossaries_path
   end
 
   private
