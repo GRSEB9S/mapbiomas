f0acaccbeb7e470da178573dcf1ccf5025086831
Gabriel Packer <ggkpacker@gmail.com>
2018-08-07 10:30:26 -0300

    Filter glossaries by locale

--- app/controllers/glossaries_controller.rb
+++ app/controllers/glossaries_controller.rb
@@ -1,5 +1,5 @@
 class GlossariesController < ApplicationController
   def index
-    @glossaries = Glossary.paginate(page: params[:page])
+    @glossaries = Glossary.paginate(page: params[:page]).where(locale: cookies[:locale])
   end
 end
