d6e18c37f9a196a33ebb1dd619ff044a6efe4d39
Gabriel Packer <ggkpacker@gmail.com>
2018-08-08 10:19:52 -0300

    Store user location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   before_action :check_maintenance
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_language
+  before_action :store_user_location!, if: :storable_location?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -21,6 +22,20 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+  #    infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
 
   def check_maintenance
     render file: 'public/maintenance', layout: false if ENV['MAINTENANCE_MODE']
