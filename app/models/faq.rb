84d9acbd4f28b5cbdbf536bc372ad96bb93db2e4
Gabriel Packer <ggkpacker@gmail.com>
2018-08-09 11:24:03 -0300

    Add ordination to faqs

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,4 +1,5 @@
 class Faq < ActiveRecord::Base
   validates :question, presence: true, uniqueness: { scope: :locale }
   validates :answer, presence: true
+  validates :ordination, presence: true
 end
