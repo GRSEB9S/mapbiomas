cfba3a2c43f51cb90ce642eaf3153e57713448ee
karydja <karydjasouza@outlook.com>
2018-08-31 13:24:08 -0300

    Load global dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,1 @@
-//= require jquery
-//= require jquery_ujs
-//= require routes
-//= require react_ujs
-//= require i18n.js
-//= require i18n/translations
 //= require bundle
---
+++ app/assets/javascripts/global_dependencies.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require routes
+//= require react_ujs
+//= require i18n.js
+//= require i18n/translations
