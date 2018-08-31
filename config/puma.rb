33ac5904fa86d4b4de88a3aa4783e3b7df3f07ef
karydja <karydjasouza@outlook.com>
2018-08-31 13:41:37 -0300

    Puma config for production

--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,10 @@ rackup DefaultRackup
 port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+if ENV['APPLICATION_HOST'] == 'mapbiomas.org'
+  bind "unix:///dados/apps/mapbiomas/tmp/sockets/puma.sock"
+end
+
 on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
